116cb022820964e19fc1014f8f9e143e825c11de
Karl Sims <karlsims.uk@gmail.com>
2017-09-14 00:08:29 +0100

    inital commit

---
+++ content.js
@@ -0,0 +1,171 @@
+/*===========================================
+* * *     Je Suis Mark Zuckerberg       * * * 
+=============================================
+
+"Je Suis Mark Zuckerberg" is a silly chrome extension that customises your
+Facebook experience by replacing all your friends profile images and names 
+with Mark Zuckerberg.
+
+Aim:
+    Try to replace the existance of all usernames and profile images with
+    Mark Zuckerberg within the Facebook DOM.
+
+TODO:
+    *   Update selector lists to be more page specific, as to reduce the 
+        number of active MutationObservers. This will require editing the
+        Mark.zuckify method to filter out un-needed selections also.
+
+    *   Refine selector locations to be more element specific and hunt out
+        un-applied profile images and usernames.
+
+    *   Correctly label with comments all the DOM selectors.
+
+    *   Re-write without jQuery dependency.
+*/
+"use strict";
+
+$(document).ready(function() {
+    var mark = new Mark();
+    mark.zuckify();
+});
+
+
+const MarkZuckerberg = "Mark Zuckerberg",
+      MarkImage128   = chrome.extension.getURL("mark_xoxo/mark128.png"),
+      MarkImage512   = chrome.extension.getURL("mark_xoxo/mark512.png");
+
+
+function Mark() {
+    /* Mark xoxoxoxo */
+    this.pageurl = window.location.href;
+
+    this.zuckify = function() {
+        replaceUsernames(fbNameLocations);
+        replaceUserThumbnails(fbProfileThumbs);
+
+        // Are we at the homepage or somewhere else?
+        if (!this.pageurl.endsWith(".com/")) {
+            replaceUserProfileImages(fbProfilePicture);
+        }
+    }
+
+    function replaceUserProfileImages(locations) {
+        for (var i = 0; i < locations.length; i++) {
+            ready(locations[i], function(element) {
+                $(element).attr('src', MarkImage512);
+            });
+        }
+    }
+
+    function replaceUserThumbnails(locations) {
+        for (var i = 0; i < locations.length; i++) {
+            ready(locations[i], function(element) {
+                $(element).attr('src', MarkImage128);
+            });
+        }
+    }
+
+    function replaceUsernames(locations) {
+        for (var i = 0; i < locations.length; i++) {
+            ready(locations[i], function(element) {
+                $(element).text(MarkZuckerberg);
+            });
+        }
+    }
+}
+
+/* Selector locations. *
+========================
+These are probally going to change pretty often. 
+*/
+
+// fbNameLocations :
+// Locations of text affected by replaceUsernames().
+var fbNameLocations = [
+    ".fwb.fcg a",                   // Feed post header.
+    ".fwb a",                       // Feed post header.
+    "a.profileLink",                // 
+    "a.UFICommentActorName",        // Feed post comments.
+    "._55lr",                       // Right sidebar contact.
+    "#fb-timeline-cover-name",      //
+    ".titlebarText.fixemoji span",  //
+    ".author.fixemoji span",        // Blue navigation messages drop-down.
+    ".fbRemindersTitle strong",     // reminders like birthdays.
+    "div._4l_v span.fwb",           // notifications dropdown.
+    "a._zci",                       // name on videos pictures ?
+    ".tooltipText span",            // pop ups ?
+
+    "._33vv a",                     // Page Name.
+];
+
+// fbProfileThumbs : 
+// Locations of images affected by replaceUserThumbnails().
+var fbProfileThumbs = [
+    "img._s0._4ooo._5xib._5sq7._44ma._rw.img",  // Post thumbnail.
+    "img._s0._4ooo._5xib._44ma._54ru.img",      // Sponsored link that others liked.      
+    "img.UFIActorImage._54ru.img",              // Comments **need to edit to not replace self.**
+    "._55lt img.img",                           //
+    "._31o4._3njy img.img",                     //
+    "img._s0._4ooo.tickerStoryImage._54ru.img", // Top right notifications.
+    ".img._s0._rw.img",                         //
+]; 
+
+// fbProfilePicture : 
+// Locations of images affected by replaceUserProfileImages().
+var fbProfilePicture = [
+    "img.profilePic.img",               // main profile image normal user.
+    "img._4jhq.img",                    // Page profile pic on far left style.
+    "img._s0._4ooo._1ve7._3s6v._rv.img" // Friends list.
+];
+
+
+/* 'ready' Utility. *
+=====================
+Written by ryan morr.
+http://ryanmorr.com/using-mutation-observers-to-watch-for-element-availability/ 
+*/
+(function(win) {
+    var listeners = [], 
+        doc = win.document, 
+        MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+        observer;
+    
+    function ready(selector, fn) {
+        // Store the selector and callback to be monitored
+        listeners.push({
+            selector: selector,
+            fn: fn
+        });
+        if (!observer) {
+            // Watch for changes in the document
+            observer = new MutationObserver(check);
+            observer.observe(doc.documentElement, {
+                childList: true,
+                subtree: true
+            });
+        }
+        // Check if the element is currently in the DOM
+        check();
+    }
+    function check() {
+        // Check the DOM for elements matching a stored selector
+        for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+            listener = listeners[i];
+            // Query for elements matching the specified selector
+            elements = doc.querySelectorAll(listener.selector);
+            for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+                element = elements[j];
+                // Make sure the callback isn't invoked with the 
+                // same element more than once
+                if (!element.ready) {
+                    element.ready = true;
+                    // Invoke the callback with the element
+                    listener.fn.call(element, element);
+                }
+            }
+        }
+    }
+    // Expose `ready`
+    win.ready = ready; 
+})(this);
+
